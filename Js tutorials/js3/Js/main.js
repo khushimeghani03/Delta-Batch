becabd527c83b76d2ec0ece5ba6471e43adcb3e2
Khushi <khushimeghani17@gmail.com>
2024-04-25 21:13:01 +0530

    new file

--- Js tutorials/js3/Js/main.js
+++ Js tutorials/js3/Js/main.js
@@ -2,4 +2,31 @@
 const myVariable = "mathematics";
 // The Length Property
 console.log("Today is Tuesday".length);
-
+var a;
+console.log(a);
+var b = 7; 
+a =b;
+console.log(b);
+var x = 4;
+var y = 8;
+var z = "Khushi";
+ x =  x+ 1;
+ y = y+ 4;
+ z = z + " String!!";
+ console.log(x);
+ console.log(y);
+ console.log(z);
+ var s = "i am a \"double qouted\" string in \"double qoutes\"";
+ console.log(s);
+ 
+ var k = 'i am a "double qouted" string in "double qoutes"';
+ console.log(k);
+ var a = "Khushi";
+ var b = " is well";
+ a+=b;
+ console.log(a);
+ var firstlength =0;;
+ var first = "Hii , I am doing well";
+ firstlength= first.length;
+ console.log(firstlength);
+ console.log(first.length);
