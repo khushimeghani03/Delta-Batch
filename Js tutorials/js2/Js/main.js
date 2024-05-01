8644f7005dd9579085e9b2fae20343bcb2608a02
Khushi <khushimeghani17@gmail.com>
2024-05-01 15:59:24 +0530

    new

--- Js tutorials/js2/Js/main.js
+++ Js tutorials/js2/Js/main.js
@@ -1,5 +1,21 @@
 console.log("hello World!!");
-console.log(typeof "khushi");
-console.log(typeof 56);
-console.log(typeof true);
-console.log(typeof false);
+// console.log(typeof "khushi");
+// console.log(typeof 56);
+// console.log(typeof true);
+// console.log(typeof false);
+// function trueOrFalse(wasthattrue){
+//     if(wasthattrue){
+//         return "Yes, that was True";
+//     }
+//     return "No, that was not true";
+// }
+// console.log(trueOrFalse(false));
+// console.log(trueOrFalse(true));
+// function comparison(a,b){
+//     if(a===b){
+//         return "Equal";
+//     }
+//     return "Nottttt Equalll";
+// }
+// console.log(comparison(17, "17"));
+
